6b619f767c2b5d8ec76f0360ff33e517a833357d
Arvîn mostafaei <Arvinmostafaeii@gmail.com>
2022-09-14 11:21:42 +0430

    add click outside hook with the help of useOnClick

---
+++ Hooks/useClickOutside.ts
@@ -0,0 +1,40 @@
+import { useCallback, useRef } from 'react';
+import { useOnClick } from './useOnClick';
+
+export type ClickOutsideOptions = {
+  blacklistClassNames?: string[];
+  blacklistElements?: string[];
+};
+const checkOptions = (event: MouseEvent, options?: ClickOutsideOptions) => {
+  if (!options) {
+    return true;
+  }
+
+  const { blacklistClassNames = [], blacklistElements = [] } = options;
+
+  const passesClassnames = (event as any).path.every((element: HTMLElement) => {
+    return blacklistClassNames.every((cn) => (element.classList ? !element.classList.contains(cn) : true));
+  });
+
+  const passesElements = blacklistElements.every((elem) => (event.target as HTMLElement)?.tagName !== elem);
+
+  return passesClassnames && passesElements;
+};
+
+export const useOnClickOutside = (fn: () => void, options?: ClickOutsideOptions) => {
+  const elementRef = useRef<HTMLElement>(null);
+
+  const handleClick = useCallback(
+    (event: MouseEvent) => {
+      if (!elementRef?.current?.contains(event.target as Node) && checkOptions(event, options)) {
+        // clicked outside the ref
+        fn();
+      }
+    },
+    [fn, options],
+  );
+
+  useOnClick(handleClick);
+
+  return elementRef;
+};
